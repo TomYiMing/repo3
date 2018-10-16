f25a69ffb9390215dd58bab1dfdc389f6d4f27ac
Tom <349060428@qq.com>
2018-10-16 20:46:58 +0800

    修改git_idea项目

--- git_idea/src/main/java/com/itheima/Test1.java
+++ git_idea/src/main/java/com/itheima/Test1.java
@@ -2,7 +2,7 @@ package com.itheima;
 
 public class Test1 {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 100; i++) {
             System.out.println("I love java");
         }
 
